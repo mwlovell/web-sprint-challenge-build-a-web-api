4b62b703cc2f35ccf74f94b28e4e2b29399fb063
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-06-11 10:59:59 -0700

    added project middleware, 8 tests passing

--- api/projects/projects-middleware.js
+++ api/projects/projects-middleware.js
@@ -1,1 +1,18 @@
-// add middlewares here related to projects
+const Projects = require('./projects-model')
+
+async function validateProjectId(req, res, next){
+    try{
+        const {id} = req.params
+        const project = await Projects.get(id)
+        if(project){
+            req.params = project
+            next()
+        } else{
+            next({status: 404, message: 'Error,this project does not exist'})
+        }
+    }catch(err){
+        next(err)
+    }
+}
+
+module.exports = {validateProjectId}
--- api/projects/projects-router.js
+++ api/projects/projects-router.js
@@ -1,5 +1,25 @@
+const Projects = require('./projects-model'); 
 const express = require('express'); 
 const router = express.Router()
 
-module.exports = router
+const {validateProjectId} = require('./projects-middleware'); 
 
+router.get("/", (req, res) => {
+    Projects.get()
+        .then(project => {
+            res.status(200).json(project)
+        })
+        .catch(err => {
+            res.status(500).json({message: 'Error, information cannot be found'})
+        })
+})
+
+router.get("/:id", validateProjectId, (req,res, next) => {
+    try{
+        res.status(200).json(req.params)
+    } catch(err){
+        next(err)
+    }
+})
+
+module.exports = router
