fb45e83419849527cedfa7f47cd57d9e6c72a8ed
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-06-11 06:43:30 -0700

    added project router initial 5 passing

--- api/server.js
+++ api/server.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const server = express();
 
-// Configure your server here
-// Build your actions router in /api/actions/actions-router.js
-// Build your projects router in /api/projects/projects-router.js
-// Do NOT `server.listen()` inside this file!
+const projectsRouter = require('./projects/projects-router'); 
+
+
+server.use(express.json()); 
+server.use('/api/projects', projectsRouter);
+ 
+
+
 
 module.exports = server;
