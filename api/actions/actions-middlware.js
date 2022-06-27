ed919072bcd18ef36cb17fd18e47e50fd4398f01
Mike Lovell <mike@Carlys-MacBook-Air.local>
2022-06-27 16:47:00 -0700

    22 tests passing

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,34 @@
-// add middlewares here related to actions
+const Actions = require('./actions-model'); 
+
+
+async function validateActionId(req, res, next){
+    try{
+        const action = await Actions.get(req.params.id)
+        if(!action){
+            res.status(404).json({message: 'Error, unable to find actions were found with ID'})
+        } else{
+            req.action = action
+            next()
+        }
+    }catch(err){
+        res.status(500).json({message: 'Error, cannot find action'})
+    }
+}
+
+async function validateAction (req, res, next){
+    const {project_id, description, notes, completed} = req.body
+    if(!project_id){
+        res.status(400).json({message: 'Error, project id is missing'})
+    }
+    if(!notes || !notes.trim){
+        res.status(400).json({message: 'Error, project notes are required'})
+    } else{
+        req.project_id = project_id
+        req.description = description.trim()
+        req.notes = notes.trim()
+        req.completed = completed
+        next()
+    }
+}
+
+module.exports = {validateActionId, validateAction }
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,1 +1,35 @@
-// Write your "actions" router here!
+const Actions = require('./actions-model'); 
+const express = require('express'); 
+const router = express.Router(); 
+
+const {validateAction, validateActionId} = require('./actions-middlware')
+
+router.get("/", (req, res) => {
+    Actions.get()
+    .then(action => {
+        res.status(200).json(action)
+    })
+})
+
+router.get('/:id', validateActionId, (req, res) => {
+    res.status(200).json(req.action)
+})
+
+router.post('/', validateAction, validateActionId, (req, res) => {
+    const newAction = req.body; 
+    Actions.insert(newAction)
+    .then(project => {
+        res.status(201).json(newAction)
+    })
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = router
